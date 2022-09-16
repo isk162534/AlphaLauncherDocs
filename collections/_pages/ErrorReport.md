f13fd0a166dad740fb903ae80067ad565dbd4c47
isk162534 <isk123456ba@gmail.com>
2022-09-16 23:28:10 +0900

    add ErrorReport

---
+++ collections/_pages/ErrorReport.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: 오류 보고서
+permalink: /errorreport
+---
+
+오류 보고서의 자세한 내용을 볼 수 있습니다. 
+
+오류 보고서의 아이디를 아래 칸에 입력하고 버튼을 누르면 됩니다. 
+
+{% include errorreport.html %}
---
+++ public/js/errorreporter.js
@@ -0,0 +1,56 @@
+const tableElement = document.querySelector("#table")
+const idElement = document.querySelector("#id")
+const submitElement = document.querySelector("#submit")
+
+function createTableRow(k, v) {
+    const tr = document.createElement("tr")
+
+    const ktd = document.createElement("td")
+    ktd.innerText = k
+    tr.appendChild(ktd)
+
+    const vtd = document.createElement("td")
+    vtd.innerText = v
+    tr.appendChild(vtd)
+
+    return tr
+}
+
+async function load(id) {
+    try {
+        submitElement.disabled = true
+
+        res = await fetch("https://api.alphabeta.pw/errorreport/report?id=" + id)
+        if (!res.ok) {
+            alert("요청 실패")
+            return
+        }
+
+        const resBody = await res.json()
+        if (!resBody || !Array.isArray(resBody)) {
+            alert("결과 없음")
+            return
+        }
+        const result = resBody[0]._source;
+
+        const rows = []
+        rows.push(createTableRow("id", id))
+        Object.entries(result).forEach(([k, v]) => {
+            const tr = createTableRow(k, v)
+            rows.push(tr)
+        })
+
+        tableElement.replaceChildren(...rows)
+    }
+    catch (e) {
+        alert("오류")
+    }
+    finally {
+        submitElement.disabled = false;
+    }
+}
+
+function onSubmit() {
+    load(idElement.value)
+    return false
+}
